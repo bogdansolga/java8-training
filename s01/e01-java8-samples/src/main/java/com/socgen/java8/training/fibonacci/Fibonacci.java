2a604f9918afd81aa032551d538702515388fae6
Bogdan Solga <bogdan.solga@gmail.com>
2017-04-14 07:20:56 +0300

    [dev] Added a few Java 8 samples

---
+++ s01/e01-java8-samples/src/main/java/com/socgen/java8/training/fibonacci/Fibonacci.java
@@ -0,0 +1,38 @@
+package com.socgen.java8.training.fibonacci;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Computing the Fibonacci numbers in imperative and functional ways
+ *
+ * @author bogdan.solga
+ */
+class Fibonacci {
+
+    static void imperative(final int until) {
+        for (int i = 1; i <= until; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    static void functional(final int until) {
+        IntStream fibonacciStream = Stream.iterate(new int[]{1, 1}, value -> new int[] {value[1], value[0] + value[1]})
+                                          .mapToInt(number -> number[0]);
+        fibonacciStream.limit(until)
+                       .forEach(number -> System.out.print(number + " "));
+    }
+
+    private static int fibonacci(int number) {
+        int first = 1;
+        int second = 1;
+        int fibonacci = first;
+        for (int i = 2; i < number; i++) {
+            fibonacci = first + second;
+            first = second;
+            second = fibonacci;
+        }
+
+        return fibonacci;
+    }
+}
---
+++ s01/e01-java8-samples/src/main/java/com/socgen/java8/training/fibonacci/FibonacciDemo.java
@@ -0,0 +1,17 @@
+package com.socgen.java8.training.fibonacci;
+
+/**
+ * Computing the Fibonacci numbers in imperative and functional programming
+ */
+public class FibonacciDemo {
+
+    public static void main(String[] args) {
+        // every number after the first two is the sum of the two preceding ones
+
+        Fibonacci.imperative(20);
+
+        System.out.println();
+
+        Fibonacci.functional(20);
+    }
+}
