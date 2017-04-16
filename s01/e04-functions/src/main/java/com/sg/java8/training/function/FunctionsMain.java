d20a37616a253702d0cc50c09d2020f1f508f295
Bogdan Solga <bogdan.solga@gmail.com>
2017-04-16 16:43:01 +0300

    [dev] Added some Function samples

---
+++ s01/e04-functions/src/main/java/com/sg/java8/training/function/FunctionsMain.java
@@ -0,0 +1,57 @@
+package com.sg.java8.training.function;
+
+import com.sg.java8.training.model.Product;
+import com.sg.java8.training.function.service.ProductService;
+
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ * A few {@link java.util.function.Function} usage samples
+ */
+public class FunctionsMain {
+
+    public static void main(String[] args) {
+        simpleFunctions();
+
+        productFunctions();
+
+        sectionFunctions();
+
+        managerFunctions();
+    }
+
+    private static void simpleFunctions() {
+        final Function<Double, String> priceDescription = price -> "The price is " + price;
+        System.out.println(priceDescription.apply(250d));
+
+        // TODO try other simple Functions - String, Boolean, ...
+    }
+
+    private static void productFunctions() {
+        final Product aFancyAppleProduct = new Product(10, "iSomething", 500);
+        final Function<Product, String> productPrinter = Product::toString;
+        productPrinter.apply(aFancyAppleProduct);
+
+        final ProductService productService = new ProductService();
+        final Set<String> descriptions = productService.getSamsungTabletDescriptions();
+
+        // using a Consumer on the retrieved items
+        descriptions.forEach(printer());
+
+        // TODO try other Product Functions
+    }
+
+    private static void sectionFunctions() {
+        // TODO try a few Section Functions
+    }
+
+    private static void managerFunctions() {
+        // TODO try a few Manager Functions
+    }
+
+    private static Consumer<String> printer() {
+        return System.out::println;
+    }
+}
---
+++ s01/e04-functions/src/main/java/com/sg/java8/training/function/service/ProductService.java
@@ -0,0 +1,31 @@
+package com.sg.java8.training.function.service;
+
+import com.sg.java8.training.model.Product;
+import com.sg.java8.training.model.Section;
+import com.sg.java8.training.bootstrap.StoreSetup;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * A simple service for managing {@link Product} entities
+ *
+ * @author bogdan.solga
+ */
+public class ProductService {
+
+    public Set<String> getSamsungTabletDescriptions() {
+        final Section tablets = StoreSetup.getDefaultStore()
+                                          .getStoreSections()
+                                          .stream()
+                                          .filter(section -> section.getSectionName().equals("Tablets"))
+                                          .findFirst()
+                                          .orElseThrow(() -> new IllegalArgumentException("There's no section named 'Tablets'"));
+
+        return tablets.getProducts()
+                      .stream()
+                      .filter(product -> product.getName().contains("Samsung"))
+                      .map(Product::toString)
+                      .collect(Collectors.toSet());
+    }
+}
