0ebc2a16d9cfd3cf0dcaf865d058abba5139549f
Bogdan Solga <bogdan.solga@gmail.com>
2017-04-17 11:52:37 +0300

    [dev] Added the initial class for the Optional and streams samples

---
+++ s02/s02e01-optional/src/main/java/com/sg/java8/training/optional/OptionalMain.java
@@ -0,0 +1,10 @@
+package com.sg.java8.training.optional;
+
+/**
+ * A few {@link java.util.Optional} usage samples
+ */
+public class OptionalMain {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ s02/s02e02-streams/src/main/java/com/sg/java8/training/streams/StreamsMain.java
@@ -0,0 +1,10 @@
+package com.sg.java8.training.streams;
+
+/**
+ * A few {@link java.util.stream.Stream}s usage samples
+ */
+public class StreamsMain {
+
+    public static void main(String[] args) {
+    }
+}
