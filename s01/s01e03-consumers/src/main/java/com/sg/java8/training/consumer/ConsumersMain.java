35d55dc167da287d7af9f0c898a0c7a1ba8dc0ad
Bogdan Solga <bogdan.solga@gmail.com>
2017-04-20 08:08:01 +0300

    [dev] Added a few more Consumer samples

--- s01/s01e03-consumers/src/main/java/com/sg/java8/training/consumer/ConsumersMain.java
+++ s01/s01e03-consumers/src/main/java/com/sg/java8/training/consumer/ConsumersMain.java
@@ -1,7 +1,7 @@
 package com.sg.java8.training.consumer;
 
-import com.sg.java8.training.model.Product;
 import com.sg.java8.training.consumer.service.ProductService;
+import com.sg.java8.training.model.Product;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,8 +23,22 @@ public class ConsumersMain {
     }
 
     private static void simpleConsumers() {
-        final Consumer<Integer> integerConsumer = number -> System.out.println(number);
+        final Consumer<Integer> integerConsumer = System.out::println;
         integerConsumer.accept(25);
+
+        final List<String> list = Arrays.asList("I want a holiday, not just a weekend".split(" "));
+
+        // imperative way
+        for (final String item : list) {
+            System.out.println(item);
+        }
+
+        // functional way
+        list.forEach(System.out::println);
+        list.forEach(item -> System.out.println(item)); // just a matter of taste :)
+
+        final Consumer<Boolean> booleanConsumer = it -> System.out.println("The boolean value is " + Boolean.valueOf(it));
+        booleanConsumer.accept(Boolean.TRUE);
 
         // TODO using Consumers as methods and with more than one statements
 
@@ -37,6 +51,10 @@ public class ConsumersMain {
         final Product product = new Product(10, "iSomething", 500);
         final Consumer<Product> productConsumer = it -> System.out.println("The selected product is " + product);
         productConsumer.accept(product);
+
+        final Consumer<Product> preProcessor = product1 -> System.out.println("Pre-processing the product...");
+        preProcessor.andThen(productConsumer)
+                    .accept(product);
 
         final ProductService productService = new ProductService();
         productService.displayAppleTablets();
