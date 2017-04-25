82ea48678d5c25bfb836365d2a7d86d5ee8bd972
Bogdan Solga <bogdan.solga@gmail.com>
2017-04-25 15:55:04 +0300

    [dev] Added a few completion stages samples

--- s03/s03e03-completable-future/src/main/java/com/sg/java8/training/completable/future/CompletableFutureMain.java
+++ s03/s03e03-completable-future/src/main/java/com/sg/java8/training/completable/future/CompletableFutureMain.java
@@ -1,24 +1,77 @@
 package com.sg.java8.training.completable.future;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 /**
  * A few {@link java.util.concurrent.CompletableFuture} usage samples
  */
 public class CompletableFutureMain {
+    private static final Executor EXECUTOR = Executors.newFixedThreadPool(5);
+    private static final ExecutorCompletionService COMPLETION_SERVICE =
+            new ExecutorCompletionService(EXECUTOR);
 
     public static void main(String[] args) {
-        final CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "a very simple text");
-        completableFuture.thenAcceptAsync(value -> System.out.println(Thread.currentThread().getName() + " - " + value));
+        //simpleCompletableFutures();
 
-        final CompletableFuture<String> anotherFuture = CompletableFuture.supplyAsync(() -> "another text");
+        CompletableFuture<String> first = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName());
+            return "first";
+        }, EXECUTOR);
+
+        CompletableFuture<String> second = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName());
+            return "second";
+        }, EXECUTOR);
+
+        CompletableFuture<Integer> third = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName());
+            return 7;
+        });
+
+        final CompletableFuture<Integer> future =
+                first.thenCompose(value -> second)
+                     .thenComposeAsync(value -> third);
+
+        System.out.println(future.join());
+
+        final CompletableFuture<Void> finalFuture = CompletableFuture.allOf(first, second);
+        finalFuture.thenAccept(value -> notifyFinishedTasks());
+
+        ((ExecutorService) EXECUTOR).shutdown();
+
+        //first.thenCombine(second, (x, y) -> x + y);
+    }
+
+    private static void notifyFinishedTasks() {
+        System.out.println(Thread.currentThread().getName() + " - All good");
+    }
+
+    private static void simpleCompletableFutures() {
+        final CompletableFuture<String> completableFuture =
+                CompletableFuture.supplyAsync(() -> "a very simple text");
+
+        final Consumer<String> stringConsumer = stringPrinter();
+        completableFuture.thenAcceptAsync(stringConsumer);
+
+        final CompletableFuture<String> anotherFuture =
+                CompletableFuture.supplyAsync(() -> "another text");
+
         completableFuture.thenCompose(value -> anotherFuture);
-        anotherFuture.thenAcceptAsync(value -> System.out.println(Thread.currentThread().getName() + " - " + value));
+        final CompletableFuture<String> completableFuture1 =
+                anotherFuture.thenApplyAsync(value -> value);
 
         completableFuture.exceptionally(throwable -> "Thrown: " + throwable.getMessage());
 
         completableFuture.thenApplyAsync(String::toUpperCase, Executors.newCachedThreadPool());
-        //completableFuture.acceptEitherA()
+        completableFuture.acceptEither(anotherFuture, stringConsumer);
+    }
+
+    private static Consumer<String> stringPrinter() {
+        return value -> System.out.println(Thread.currentThread().getName() + " - " + value);
     }
 }
