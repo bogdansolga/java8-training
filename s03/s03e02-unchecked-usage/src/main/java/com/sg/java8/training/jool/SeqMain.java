52a2bf6d82adcd395eb618b557b4454cb3527f02
Bogdan Solga <bogdan.solga@gmail.com>
2017-04-25 15:59:51 +0300

    [dev] Improved the Sequence exercises - edited message

--- s03/s03e02-unchecked-usage/src/main/java/com/sg/java8/training/jool/SeqMain.java
+++ s03/s03e02-unchecked-usage/src/main/java/com/sg/java8/training/jool/SeqMain.java
@@ -1,6 +1,8 @@
 package com.sg.java8.training.jool;
 
 import org.jooq.lambda.Seq;
+import org.jooq.lambda.tuple.Tuple2;
+
 
 /**
  * A short sample of using {@link org.jooq.lambda.Seq}
@@ -8,9 +10,23 @@ import org.jooq.lambda.Seq;
 public class SeqMain {
 
     public static void main(String[] args) {
-        final Seq<String> seq = Seq.of("some", "values", "and", "some", "others");
+        final Seq<String> seq = getSequence();
         seq.distinct()
            .sorted()
            .forEach(System.out::println);
+
+        System.out.println("------------------------------------------------------");
+
+        final String folded = getSequence().foldLeft("{", (first, second) -> first + "|" + second);
+        System.out.println(folded);
+
+        System.out.println("------------------------------------------------------");
+
+        final Seq<Tuple2<String, Long>> zipWithIndex = getSequence().zipWithIndex();
+        System.out.println(zipWithIndex);
+    }
+
+    private static Seq<String> getSequence() {
+        return Seq.of("I want a holiday, not just a weekend".split(" "));
     }
 }
