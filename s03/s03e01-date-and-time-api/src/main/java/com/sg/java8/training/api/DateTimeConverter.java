85979d98bc67aa56499af323dd5ae54e38615230
Bogdan Solga <bogdan.solga@gmail.com>
2017-05-17 15:13:33 +0300

    [clean] Removed the access modifiers

--- s03/s03e01-date-and-time-api/src/main/java/com/sg/java8/training/api/DateTimeConverter.java
+++ s03/s03e01-date-and-time-api/src/main/java/com/sg/java8/training/api/DateTimeConverter.java
@@ -6,15 +6,18 @@ import java.util.Date;
 import java.util.Objects;
 
 // DRY - don't repeat yourself
-public final class DateTimeConverter {
+final class DateTimeConverter {
 
-    public static Date convertLocalDate(final LocalDate localDate) {
+    static Date convertLocalDate(final LocalDate localDate) {
         Objects.requireNonNull(localDate);
-        return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        return Date.from(localDate.atStartOfDay(ZoneId.systemDefault())
+                                  .toInstant());
     }
 
-    public static LocalDate convertDate(final Date date) {
+    static LocalDate convertDate(final Date date) {
         Objects.requireNonNull(date);
-        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        return date.toInstant()
+                   .atZone(ZoneId.systemDefault())
+                   .toLocalDate();
     }
 }
