527b073ac5ab7e29354e699b0a449f65c78bc7c1
Bogdan Solga <bogdan.solga@gmail.com>
2017-05-17 15:18:48 +0300

    [improve] Generate a random sleep, refactored some method calls

--- s03/s03e03-completable-future/src/main/java/com/sg/java8/training/completable/future/ProductProcessor.java
+++ s03/s03e03-completable-future/src/main/java/com/sg/java8/training/completable/future/ProductProcessor.java
@@ -6,15 +6,20 @@ import com.sg.java8.training.model.Store;
 import org.jooq.lambda.Unchecked;
 
 import java.util.Collection;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 class ProductProcessor {
 
+    private static final Random RANDOM = new Random();
+
+    private static final int MAX_SLEEP_TIME = 2000;
+
     CompletableFuture<Long> getProductsStock(final String productName) {
         return CompletableFuture.supplyAsync(() -> {
             displayThreadName();
-            sleepALittle(500);
+            sleepALittle();
 
             final Store store = StoreSetup.getDefaultStore();
             return store.getStoreSections()
@@ -29,7 +34,7 @@ class ProductProcessor {
     Function<Long, CompletableFuture<Double>> getProductsPrice() {
         return productsStock -> {
             displayThreadName();
-            sleepALittle(400);
+            sleepALittle();
 
             return CompletableFuture.supplyAsync(() -> productsStock * 230d);
         };
@@ -38,18 +43,20 @@ class ProductProcessor {
     Function<Double, CompletableFuture<String>> getDisplayedText() {
         return productsPrice -> {
             displayThreadName();
-            sleepALittle(200);
+            sleepALittle();
 
-            return CompletableFuture.supplyAsync(() -> "The price of the products is " +
-                    productsPrice);
+            return CompletableFuture.supplyAsync(() -> "The price of the products is " + productsPrice);
         };
     }
 
     // TODO return a Map of the products and their stock, using a grouping collector
-
-    private void sleepALittle(final int sleepDuration) {
-        Unchecked.consumer(it -> Thread.sleep(sleepDuration))
+    private void sleepALittle() {
+        Unchecked.consumer(it -> Thread.sleep(getRandomSleepDuration()))
                  .accept(null);
+    }
+
+    private int getRandomSleepDuration() {
+        return RANDOM.nextInt(MAX_SLEEP_TIME);
     }
 
     private void displayThreadName() {
