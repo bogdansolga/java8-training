e52c4754602ee518c86e522482489f22e05b6836
Bogdan Solga <bogdan.solga@gmail.com>
2017-05-17 15:11:23 +0300

    [improve] Improved / cleaned a few samples

--- s01/s01e06-bi-functional-interfaces/src/main/java/com/sg/java8/training/bi/functional/interfaces/BiFunctionalInterfacesMain.java
+++ s01/s01e06-bi-functional-interfaces/src/main/java/com/sg/java8/training/bi/functional/interfaces/BiFunctionalInterfacesMain.java
@@ -20,18 +20,20 @@ public class BiFunctionalInterfacesMain {
     }
 
     private static void biPredicatesSamples() {
-        final BiPredicate<String, Integer> biPredicate = (first, second) -> first.length() > 10 && second > 0;
+        final BiPredicate<String, Integer> biPredicate = (first, second) ->
+                first.length() > 10 && second > 0; // whatever the business logic requires
         System.out.println(biPredicate.test("something", 5));
     }
 
     private static void biFunctionsSamples() {
-        final BiFunction<String, Integer, String> biFunction = (string, integer) -> "The value of " + string + " and " + integer;
+        final BiFunction<String, Integer, String> biFunction =
+                (first, second) -> "The value of " + first + " and " + second;
         System.out.println(biFunction.apply("another one", 20));
 
         // new methods added in the Map interface
         final Map<Integer, String> months = getMonths();
 
-        months.compute(4, (key, value) -> "April");
+        months.compute(4, (key, value) -> "April or something else");
 
         months.computeIfPresent(2, (key, value) -> "The shortest month");
 
@@ -51,15 +53,19 @@ public class BiFunctionalInterfacesMain {
         // iteration - imperative mode
         final Set<Map.Entry<Integer, String>> entries = months.entrySet();
         for (Map.Entry<Integer, String> entry : entries) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
+            processEntry(entry);
         }
 
         // iteration - functional mode
         months.forEach((key, value) -> System.out.println(key + " -> " + value)); // easier iteration
     }
 
+    private static void processEntry(final Map.Entry<Integer, String> entry) {
+        System.out.println(entry.getKey() + " -> " + entry.getValue());
+    }
+
     private static Map<Integer, String> getMonths() {
-        final Map<Integer, String> months = new HashMap<>(5);
+        final Map<Integer, String> months = new HashMap<>();
 
         months.put(1, "January");
         months.put(2, "February");
