572645f072468b7388498c04c12699006f16c103
Bogdan Solga <bogdan.solga@gmail.com>
2017-05-17 15:15:39 +0300

    [dev] Added a sample of functional vs imperative programming

---
+++ s01/s01e01-java8-samples/src/main/java/com/sg/java8/training/FunctionalVsImperativeDemo.java
@@ -0,0 +1,24 @@
+package com.sg.java8.training;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class FunctionalVsImperativeDemo {
+
+    public static void main(String[] args) {
+        final List<String> strings = Arrays.asList("I want a holiday, not just a weekend".split(" "));
+
+        // imperative processing
+        final Iterator<String> iterator = strings.iterator();
+        while (iterator.hasNext()) {
+            String currentValue = iterator.next();
+            if (currentValue.isEmpty()) {
+                iterator.remove();
+            }
+        }
+
+        // functional processing
+        strings.removeIf(value -> value.isEmpty());
+    }
+}
