80fc4ac2b6f631c85e5b32d8606843c0a0a98f0c
Bogdan Solga <bogdan.solga@gmail.com>
2018-03-05 10:00:21 +0200

    [improve] Extracted a few methods

--- s01/s01e02-predicates/src/main/java/com/sg/java8/training/predicate/service/ProductService.java
+++ s01/s01e02-predicates/src/main/java/com/sg/java8/training/predicate/service/ProductService.java
@@ -6,7 +6,6 @@ import com.sg.java8.training.bootstrap.StoreSetup;
 import com.sg.java8.training.model.StoreSection;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -18,16 +17,28 @@ import java.util.stream.Collectors;
 public class ProductService {
 
     public List<Product> getNexusTablets() {
-        final Section tablets = StoreSetup.getDefaultStore()
-                                          .getStoreSections()
-                                          .stream()
-                                          .filter(tabletsSection())
-                                          .findFirst()
-                                          .orElseThrow(() -> new IllegalArgumentException("There's no section named 'Tablets'"));
+        final Section tablets = getTablets();
 
-        final List<Product> products = tablets.getProducts()
-                                              .orElseThrow(() -> new IllegalArgumentException("There are no available tablets"));
+        final List<Product> products = getProductsOrThrow(tablets);
 
+        return getNexusTablets(products);
+    }
+
+    private Section getTablets() {
+        return StoreSetup.getDefaultStore()
+                         .getStoreSections()
+                         .stream()
+                         .filter(tabletsSection())
+                         .findFirst()
+                         .orElseThrow(() -> new IllegalArgumentException("There's no section named 'Tablets'"));
+    }
+
+    private List<Product> getProductsOrThrow(final Section tablets) {
+        return tablets.getProducts()
+                      .orElseThrow(() -> new IllegalArgumentException("There are no available tablets"));
+    }
+
+    private List<Product> getNexusTablets(final List<Product> products) {
         return products.stream()
                        .filter(product -> product.getName().toLowerCase().contains("nexus"))
                        .collect(Collectors.toList());
